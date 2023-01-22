840e17568939606e4028f3270d9984f7ea72e603
Ioofy <mynev.id@gmail.com>
2023-01-22 14:09:03 +0700

    adding another example algorithms

---
+++ playgrounds/algorithms/mergeDuplicate.ts
@@ -0,0 +1,30 @@
+const productObj = [
+  {
+    id: 1,
+    productName: "Camera X2Z Sonny",
+    price: "2000000",
+    tag: "Camera",
+    location: "North Jakarta City",
+  },
+  {
+    id: 2,
+    productName: "Samsung Note 8+",
+    price: "2800000",
+    tag: "HP",
+    location: "North Jakarta City",
+  },
+  {
+    id: 3,
+    productName: "Macbook Air M2",
+    price: "20000000",
+    tag: "Laptop",
+    location: "Bogor City",
+  },
+];
+
+// Expected:
+// The value productObj with same location will merge
+
+const mergeDuplicate = () => {
+  return;
+};
